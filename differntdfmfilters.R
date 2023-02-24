9e21905a4a3efe47a5d1ff0262a7785042895dd4
powlinefried <122636212+powlinefried@users.noreply.github.com>
2023-02-24 12:36:05 +0100

    Update differntdfmfilters.R

--- differntdfmfilters.R
+++ differntdfmfilters.R
@@ -1,8 +1,10 @@
 options(stringsAsFactors = FALSE)
 library(quanteda)
+library(stringr)
 
-#Baustellen in diesem Skript: SchülerInnen wird nicht gefunden, wegen lowercasing the tokens
+#Baustellen in diesem Skript: SchülerInnen wird nicht gefunden, wegen lowercasing the tokens, sowie frauen*
 #                             word boundaries bei doppelnennung sind nicht im reg ex, dadurch false positives
+
 
 #this script is used to filter the dtm's by differnt forms of gendered language
 #1. the first part filters a dtm, for a list of words of gendered language created by
@@ -10,32 +12,43 @@ library(quanteda)
 #2. the second part of the scrpit filters the dtm for a list of reg ex (*innen etc)
 #3. the third part of the script filters for the usage of Doppelnennung of female and male forms
 #(Leher und Lehrerinnen)
-#the last part of the list filters a dtm by all three measures(???)
+
 
 
 #--------------------1: filter dtm for list of gendered words:
 
-#load gender list
-genderworddoc<- read.csv("gendered_words_splitted.csv")
-#create list of words
-genderwordlist<-c(genderworddoc1[["gendered_words_splitted"]])
 
 
-#filter a DTM by the genderwordlist
-dtm_filtered_1<-dfm_select(taz_DTM,
-  pattern = genderwordlist,
-  selection ="keep",padding = FALSE)
+#function that filters a given DTM through filter 1 and returns the reduced DTM
 
-#create new dtm sums for Loglikelyhoodtest
+filter1 <- function (DTM){
+  
+  #load gender list
+  genderworddoc<- read.csv("gendered_words_splitted.csv")
+  #create list of words
+  genderwordlist<-c(genderworddoc1[["gendered_words_splitted"]])
+  
+  #filter a DTM by the genderwordlist
+  dtm_filtered_1<-dfm_select(DTM,
+                             pattern = genderwordlist,
+                             selection ="keep",padding = FALSE)
+  
+  #create new dtm sums for Loglikelyhoodtest
+  
+  sum_dtm_filtered_1 <- colSums(dtm_filtered_1)
+  
+  return(sum_dtm_filtered_1)
+}
 
-sum_dtm_filtered_1 <- colSums(dtm_filtered_1)
+filter1(taz_DTM)
+
 
 #------------------------------2: Filter dtm by reg Ex list
 
 # in the tokenization the * symbol is put as a single token, 
 #: stays in the middle of the word, 
 #() become single tokens
-#/\ become single tokens
+#/ become single tokens
 # - stay in the middle of the word
 #Capitalletters are lowercased
 
@@ -43,17 +56,17 @@ sum_dtm_filtered_1 <- colSums(dtm_filtered_1)
 
 
 # Here is a sample Text to text how to do that
-sample <- "hus_in Schulerin und Schuler schüler und leher my name is Schüler·innen Christina. 50 Sometimes Lehrer/in we get some weirdness
-Hello my name is Michael:in, 
-sometimes schmim(innen) we get some Sculer und Lehereinnen weird,and odd, results-- 50 I  want to hund und katin replace the 
- 50s"
-corpus<-corpus(sample)
-toks<-tokens(corpus)
+#sample <- "hus_in Schulerin und Schuler schüler und leher my name is Schüler·innen Christina. 50 Sometimes Lehrer/in we get some weirdness
+#Hello my name is Michael:in, 
+#sometimes schmim(innen) we get some Sculer und Lehereinnen weird,and odd, results-- 50 I  want to hund und katin replace the 
+# 50s"
+#corpus<-corpus(sample)
+#toks<-tokens(corpus)
 
 
-applyfilter2 <- function(tokens) {
+adjusttokens_filter2 <- function(Tokens) {
   #take care of the : - · _ cases, by splitting tokens:
-  symbolsplit <- tokens_split(toks, separator = ":",
+  symbolsplit <- tokens_split(Tokens, separator = ":",
                               valuetype = c("fixed", "regex"),
                               remove_separator = FALSE)
   
@@ -70,7 +83,7 @@ applyfilter2 <- function(tokens) {
                               remove_separator = FALSE)
   
   #take care of the * / () : - · _cases by compunding:
-  taki<- tokens_compound(symbolsplit, list(c("*", "in"), c("*", "innen"), c("/","in"), 
+  adjustedtokens<- tokens_compound(symbolsplit, list(c("*", "in"), c("*", "innen"), c("/","in"), 
                                            c("/","innen"), c("(","in",")"),
                                            c("(","innen",")"),c(":", "in"), c(":", "innen"),
                                            c("-", "in"), c("-", "innen"),
@@ -82,44 +95,62 @@ applyfilter2 <- function(tokens) {
   #Offen: take care of the I and Frauen* (lowercasing): "[A-Z][a-z]+(In|Innen)", "[A-Z][a-z]+*
   
   
-  return(taki)
+  return(adjustedtokens)
   
 }
 
 
-#apply these steps to the tokens of the corpurs
-tokens_filter2<- applyfilter2(toks)
+filter2 <- function (NewTokens){
+  
+  #apply these steps to the tokens of the corpurs
+ 
 
-#create dtm out of the new tokens
+  #create dtm out of the new tokens
+  
+  filter2_dtm <- dfm(NewTokens)
+  
+  
+  #filter a DTM by the regexlist
+  dtm_filtered_2<-dfm_select(filter2_dtm,
+                             pattern = c("*_in","*_innen",":_in",":_innen", "(_in_)", "(_innen_)", 
+                                         "·_in", "·_innen", "__in", "__innen", "/_in", "/_innen", "-_in", "-_innen"),
+                             selection ="keep")#again, something does not work with the reg exes
+  
+  #sum up columns for loglikelyhod calculation
+  
+  sum_dtm_filtered_2 <- colSums(dtm_filtered_2)
+  
+  return(sum_dtm_filtered_2)
+}
 
-filter2_dtm <- dfm(tokens_filter2)
-print(filter2_dtm)
-
-#filter a DTM by the regexlist
-dtm_filtered_2<-dfm_select(filter2_dtm,
-                           pattern = c("*_in","*_innen",":_in",":_innen", "(_in_)", "(_innen_)", 
-                          "·_in", "·_innen", "__in", "__innen", "/_in", "/_innen", "-_in", "-_innen"),
-                           selection ="keep")#again, something does not work with the reg exes
-
-#sum up columns for loglikelyhod calculation
-
-sum_dtm_filtered_2 <- colSums(dtm_filtered_2)
+#test
+filter2(toks)
 
 
 #------------------------------3: Filter dtm by doppelnennung
 
+#function that takes a corpus, applies filter 3, and gives out a NUMBER of matches
 
-#find all doppelnennung in a corpus
-doppelnennung <- str_match_all(taz_corpus,"([A-Z][a-z]* und [A-Z][a-z]*(innen|in))|([A-Z][a-z]*(innen|in) und [A-Z][a-z]*)")
-#the regex should have a word boundary to exclude cases like "hallo und diskrimnin", but somehow i do not manage
+filter3 <- function (corpus){
+  
+  #find all doppelnennung in a corpus
+  doppelnennung <- str_match_all(corpus,"([A-Z][a-z]* und [A-Z][a-z]*(innen|in))|([A-Z][a-z]*(innen|in) und [A-Z][a-z]*)")
+  #the regex should have a word boundary to exclude cases like "hallo und diskrimnin", but somehow i do not manage
+  
+  print(doppelnennung)
+  #no dtm should be created because for the log likelihood, target und reference corpora will 
+  # not have many intersections. However, one could use just the sums of reg exes found for both
+  #and calculate loklikelyhood with this
+  
+  num_doppelnennung <- str_count(corpus,"([A-Z][a-z]* und [A-Z][a-z]*(innen|in))|([A-Z][a-z]*(innen|in) und [A-Z][a-z]*)")
+  filter3sum <-sum(num_doppelnennung)
+  
+  
+  return(filter3sum)
+}
 
-#no dtm should be created because for the log likelihood, target und reference corpora will 
-# not have many intersections. However, one could use just the sums of reg exes found for both
-#and calculate loklikelyhood with this
-
-num_doppelnennung <- str_count(taz_corpus,"([A-Z][a-z]* und [A-Z][a-z]*(innen|in))|([A-Z][a-z]*(innen|in) und [A-Z][a-z]*)")
-filter3sum <-sum(num_doppelnennung)
-
+#test
+filter3(taz_corpus)
 
 #notes
 
@@ -140,6 +171,5 @@ filter3sum <-sum(num_doppelnennung)
                                     #filter3=       ))
 
 #all_filtered_dtm <- dfm_lookup(taz_DTM,full_filter_dtm,nomatch = "unmatched")
-
 
 
