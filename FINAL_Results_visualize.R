34a251c3385a2c4f24f01387857cdeb84a5cfe86
powlinefried <122636212+powlinefried@users.noreply.github.com>
2023-02-28 12:46:28 +0100

    Update and rename Results_visualize.R to FINAL_Results_visualize.R

---
+++ FINAL_Results_visualize.R
@@ -0,0 +1,185 @@
+install.packages("ggplot2")             #für Barcharts
+library(ggplot2)
+library(tidyr)
+install.packages("wordcloud2")          # Für Wordclouds
+library(wordcloud2)
+install.packages("data.table")           # für Tables
+library("data.table")
+
+#----------dieses script visualisiert die term frequncy der filter und gibt die Logliklihood table.
+#----------------------------#Results: filter2
+
+#als erstes werden die Ergebnisse durch die gesamt Tokens geteilt und dann multipliziert für relative Häufigkeit
+comparison2<-termCountsComparison2/sumalterms_comparison*100000
+faz2<-termCountsfaz2/sumalterms_faz*100000
+taz2<-termCountstaz2/sumalterms_taz*100000
+spiegel2<-termCountsspiegel2/sumalterms_spiegel*100000
+#sueddeutsche2<-termCountssueddeutsche2/sumalterms_sueddeutsche*100000
+welt2 <-termCountswelt2/sumalterms_welt*100000
+
+#----------------visualization for filter 2 (barchart)
+
+
+# Create a named list with all the data
+data_list2 <- list(comparison2, faz2, spiegel2, welt2, taz2)
+names(data_list2) <- c("Vergleichskorpora", "FAZ", "Spiegel", "Welt", "TAZ")
+
+# Get all the unique names across all lists
+all_names2 <- unique(unlist(lapply(data_list2, names)))
+
+# Create a data frame with counts for each name in each list
+counts_df2 <- data.frame(Name = all_names2,
+                        sapply(data_list2, function(x) {
+                          x[all_names2]
+                        }))
+
+# Reshape the data frame into long format
+
+counts_df_long2 <- gather(counts_df2, key = "Zeitung", value = "Count", -Name)
+
+# Create the bar chart
+ggplot(counts_df_long2, aes(x = Name, y = Count, fill = Zeitung)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Nutzung verschiedener Genderzeichen in deutschen Tageszeitungen",
+       x = "Genderzeichen",
+       y = "Anzahl/Gessamtmenge*100.000") +
+  theme_minimal()
+
+#------------------Loglikelihood, Visualisierung filter 2 (table)
+
+data_listll2 <- list(fazll2, spiegelll2, weltll2, tazll2)
+names(data_listll2) <- c("FAZ", "Spiegel", "Welt", "TAZ")
+
+# Get all the unique names across all lists
+all_namesll2 <- unique(unlist(lapply(data_listll2, names)))
+
+# Create a data frame with counts for each name in each list
+counts_dfll2 <- data.frame(Genderzeichen = all_namesll2,
+                         sapply(data_listll2, function(x) {
+                           x[all_namesll2]
+                         }))
+
+
+
+print(setDT(counts_dfll2))
+
+
+#-----------------------visualization Filter3 (Barchart)
+
+
+
+comparison3<-termCountsComparison3/sumalterms_comparison*100000
+faz3<-termCountsfaz3/sumalterms_faz*100000
+taz3<-termCountstaz3/sumalterms_taz*100000
+spiegel3<-termCountsspiegel3/sumalterms_spiegel*100000
+#sueddeutsche3<-termCountssueddeutsche3/sumalterms_sueddeutsche*100000
+welt3<-termCountswelt3/sumalterms_welt*100000
+
+
+
+# Create a named list with all the data
+data_list3 <- list(comparison3, faz3, spiegel3, welt3, taz3)
+names(data_list3) <- c("Vergleichskorpus", "FAZ", "Spiegel", "Welt", "TAZ")
+
+df3 <- data.frame(Zeitung = names(data_list3),
+                 Doppelnennung = unlist(data_list3))
+
+ggplot(df3, aes(x = Zeitung, y = Doppelnennung, fill = Zeitung)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Doppelnennung", y = "Anzahl/Gessamtmenge*100.000") +
+  theme_minimal()
+#-------------------------------Loglikelihood filter 3 (einfach table?)
+
+# Create a named list with all the data
+data_listll3 <- list(fazll3, spiegelll3, weltll3, tazll3)
+names(data_listll3) <- c("FAZ", "Spiegel", "Welt", "TAZ")
+
+dfll3 <- data.frame(Zeitung = names(data_listll3),
+                  Doppelnennung = unlist(data_listll3))
+
+print(setDT(dfll3))
+
+#------------------------------filter 1 Visualisieurng: 
+
+
+#Bevor wir an die Loglikelihood Visualisierung gehen, sortieren wir auch die Liste vor:
+#nach > 0 und
+#kurze qualitative analyse: welche signifikanten Wörter machen Sinn?
+
+filtered_listtaz_ll1 <- tazll1[tazll1 > 0] #für sinnvoll befunden: besuchsperson,studierende, moderation, teilnahmeliste, forschende, lesende, beratende, backende, teilnehmende, redeliste, abteilungsleitung
+filtered_listfaz_ll1 <- fazll1[fazll1 > 0] #studierende, führungskraft, versuchsperson, schreibende
+filtered_listspiegel_ll1 <- spiegelll1[spiegelll1 > 0] #redaktion, erzählende, teamleitung
+filtered_listwelt_ll1 <- weltll1[weltll1 > 0] #"studierende", "redaktion","teilnehmende","anwesende", "lehrkräfte", "lernende", "mitglieder", "lesende", "lehrende","befragte", "führungskraft", "teamleitung", "promovierende", "redepult", "hauptfigur"
+#filtered_listsueddeutsche_ll1 <- sueddeutschell1[sueddeutschell1 > 0] #person, mitarbeitende, fachkraft, kunstschaffende, klientel, moderation, beteiligte, vorsitzende, freiwillige, lernende, abgeordnete
+
+#filter den vector für dies "sinnhaften wörter
+filtered_vec_taz <- filtered_listtaz_ll1[c("besuchsperson", "studierende", "moderation", "teilnahmeliste", "forschende", "lesende", "beratende", "backende", "teilnehmende", "redeliste", "abteilungsleitung")]
+
+filtered_vec_faz <- filtered_listfaz_ll1[c("studierende", "führungskraft", "versuchspersonen", "schreibende")]
+
+filtered_vec_spiegel <- filtered_listspiegel_ll1[c("redaktion", "erzählende", "teamleitung")]
+
+filtered_vec_welt <- filtered_listwelt_ll1[c("studierende", "redaktion","teilnehmende","anwesende", "lehrkräfte", "lernende", "mitglieder", "lesende", "lehrende","befragte", "führungskraft", "teamleitung", "promovierende", "redepult", "hauptfigur")]
+
+#filtered_vec_sueddeutsche <- filtered_listsueddeutsche_ll1[c("person", "mitarbeitende", "fachkraft", "kunstschaffende", "klientel", "moderation", "beteiligte", "vorsitzende", "freiwillige", "lernende", "abgeordnete")]
+
+
+#----Loglikelihood, Visualisierung filter 1 (table)
+
+data_listll1 <- list(filtered_vec_faz, filtered_vec_spiegel, filtered_vec_welt, filtered_vec_taz)
+names(data_listll1) <- c("FAZ", "Spiegel", "Welt", "TAZ")
+
+# Get all the unique names across all lists
+all_namesll1 <- unique(unlist(lapply(data_listll1, names)))
+
+# Create a data frame with counts for each name in each list
+counts_dfll1 <- data.frame(Neutralisierung = all_namesll1,
+                           sapply(data_listll1, function(x) {
+                             x[all_namesll1]
+                           }))
+
+
+print(setDT(counts_dfll1))
+
+
+
+#jetzt noch die wordcounts Visualisieren: 
+#Aber erst, wieder nach sinnvollen Wörtern schauen und filtern
+
+filtered_termCounttaz1 <- termCountstaz1[c("arbeitende", "besuchsperson", "fachkraft", "erwerbstätige", "studierende", "betroffene","leserschaft", "pflegekraft", "zielgruppe", "handelnde", "geschäftsleitung", "moderation", "teilnahmeliste", "forschende","angestellte", "lesende", "hauptfigur", "beratende","mitglieder", "backende","mitarbeitende", "teilnehmende", "redeliste","assistenz", "abteilungsleitung","auszubildende","sprechende")] 
+
+filtered_termCountfaz1 <- termCountsfaz1[c("studierende","mitglied","mitglieder", "führungskraft","zielgruppe","geschäftsführung","fachkraft","vorstandsmitglied","mitarbeitende","teilnehmende", "gesetzgebende", "versuchspersonen","angehörige","personal","schreibende","belegschaft", "schulleitung","pflegepersonal", "redepult", "fachkräfte","fachleute","testpersonen","moderation","befragte","eheleute","pflegende", "schreibende")]
+
+filtered_termCountspiegel1 <- termCountsspiegel1[c("lehrende", "leitung", "moderierende", "studierende", "mitglieder", "teamleitung", "lehrkräfte", "mitglied", "pflegekraft", "beschäftigte", "forschende", "führungskraft","teamleitung")]
+
+filtered_termCountwelt1 <- termCountswelt1[c("studierende", "teilnehmende", "leitung", "anwesende", "lehrkräfte", "lernende",  "studierendenwerk", "mitglieder", "fachkräfte", "zielgruppe",  "lesende", "mitarbeitende",  "stadtoberhaupt", "versuchspersonen", "mitglied", "lehrende", "befragte", "elternteil", "führungskraft", "vorstand", "leserschaft",  "lehrkraft", "teamleitung", "betroffene",  "belegschaft", "redeliste", "personal", "beschäftigte", "führung", "fachleute", "teilnahmeliste", "pflegekraft", "vorsitz", "arbeitskraft", "promovierende", "ehrenamtliche", "feuerwehrleute", "redepult", "moderation", "pflegekräfte", "hauptfigur")]
+
+#filtered_terCountsueddeutsche1 <- termCountssueddeutsche1[c("mitarbeitende", "fachkraft", "kunstschaffende",  "moderation", "beteiligte", "vorsitzende", "lernende")]#ohne "person", "klientel","freiwillige", , "abgeordnete"
+
+
+#dann die relevanten terms der jeweiligen zeitungen realtivieren mit: geteilt durch die gesamttermzahl, *100000
+faz1<-filtered_termCountfaz1/sumalterms_faz*100000
+taz1<-filtered_termCounttaz1/sumalterms_taz*100000
+spiegel1<-filtered_termCountspiegel1/sumalterms_spiegel*100000
+#sueddeustche1<-filtered_terCountsueddeutsche1/sumalterms_sueddeutsche*100000
+welt1<-filtered_termCountwelt1/sumalterms_welt*100000
+
+
+# jetzt Visulaisierung: z.b wordclouds
+
+df_taz1 <- data.frame(word = names(taz1), count = taz1, row.names = NULL)
+wordcloud2(df_taz1, shuffle = F, size = 0.2)
+
+df_faz1 <- data.frame(word = names(faz1), count = faz1, row.names = NULL)
+wordcloud2(df_faz1, shuffle = F, size = 0.2)
+
+df_spiegel1 <- data.frame(word = names(spiegel1), count = spiegel1, row.names = NULL)
+wordcloud2(df_spiegel1, shuffle = F, size = 0.2)
+
+df_welt1 <- data.frame(word = names(welt1), count = welt1, row.names = NULL)
+wordcloud2(df_welt1, shuffle = F, size = 0.2)
+
+
+
+
+
